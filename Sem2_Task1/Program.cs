7bfabca049fcf78a864d8f8335db90f3958c932a
Irina Nistratova <nirikha@mail.ru>
2023-02-19 20:25:21 +0300

    Sem2

---
+++ Sem2_Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿
+System.Random numSintezator = new System.Random();
+
+//Вариант 1
+int rndNumber = numSintezator.Next(10,100);
+Console.WriteLine(rndNumber);
+int firstNum = rndNumber/10;
+int secondNum = rndNumber%10;
+
+if(firstNum>secondNum)
+{
+    Console.WriteLine(firstNum);
+}
+else
+{
+    Console.WriteLine(secondNum);
+}
+
+//Вариант 2
+char[] digits = numSintezator.Next(10,100).ToString().ToCharArray();
+Console.WriteLine(digits);
+int firstNumber = ((int)digits[0])-48;
+int secondNumber = ((int)digits[1])-48;
+
+//тернарный оператор
+//<переменная> = (условие)?<значение1>:<значение2>;
+//if(условие)
+// {
+//    <переменная> = <значение1>;
+// }
+// else
+// {
+//    <переменная> = <значение2>;  
+// }
+
+int resultNumber = firstNumber>secondNumber?resultNumber=firstNumber:resultNumber=secondNumber;
+Console.WriteLine(resultNumber);
---
+++ Sem2_dz2/Program.cs
@@ -0,0 +1,36 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+//что третьей цифры нет.
+
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+//* Сделать вариант для числа длиной до 10 цифр не используя char или string
+
+// int num;
+// int numToCharArray;
+
+// Console.WriteLine("Введите число ");
+// num = Convert.ToInt32(Console.ReadLine());
+// char[]array = numToCharArray();
+
+// Console.WriteLine(array[2]);
+
+int num1;
+int num2;
+
+Console.WriteLine("Введите число ");
+
+num1 = Convert.ToInt32(Console.ReadLine());
+if(num1>=100 && num1<=999)
+{
+    num2 = num1%10;
+    Console.WriteLine(num2);
+}
+
+if(num1<=99)
+{
+    Console.WriteLine("Третьей цифры нет ");
+}
+
+
+

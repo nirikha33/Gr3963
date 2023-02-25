0f94fee66fac694fff584171164403973002069d
Irina Nistratova <nirikha@mail.ru>
2023-02-25 21:56:14 +0300

    Sem4

---
+++ Sem4_Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает число, 
+// а на выходе показывает количество цифр в данном числе.
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return (int.Parse(Console.ReadLine() ?? "0"));
+}
+
+int NumberOfDigits(int num)
+{
+    return (int)(Math.Floor(Math.Log10(num))) + 1;
+}
+
+void PrintAnswer(string explain, int num)
+{
+    Console.WriteLine(explain + num);
+}
+
+int num = ReadData("Введите число: ");
+PrintAnswer("Количество цифр в данном числе: ", NumberOfDigits(num));
---
+++ Sem4_dz1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+// *Написать калькулятор с операциями +, -, /, * и возведение в степень
+
+int numA;
+int numB;
+
+// выводим сообщение в консоль и считываем данные
+Console.WriteLine("Введите число: ");
+numA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите степень: ");
+numB = Convert.ToInt32(Console.ReadLine());
+
+// применяем метод Math.Pow
+Math.Pow(numA, numB);
+
+// выводим сообщение в консоль
+Console.WriteLine($"Число {numA} в степени {numB} равно: " +Math.Pow(numA, numB));
